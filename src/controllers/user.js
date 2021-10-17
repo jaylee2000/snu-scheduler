4f0b6b6696cf5aacd4cc4181296342d626cdfe60
LJJ <jaylee2000@snu.ac.kr>
2021-10-17 01:51:50 +0000

    MVC structure for userRotues

---
+++ src/controllers/user.js
@@ -0,0 +1,37 @@
+module.exports.renderRegisterForm = (req, res) => {
+    res.render('users/register');
+}
+
+module.exports.registerUser = async (req, res, next) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username }); // Failing here
+        const registeredUser = await User.register(user, password);
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+			// console.log("Register success!");
+            res.redirect('/');
+        })
+    } catch (e) {
+		// console.log("Register failure!");
+        res.redirect('/register');
+    }
+}
+
+module.exports.renderLoginForm = (req, res) => {
+    res.render('users/login');
+}
+
+module.exports.loginUser = (req, res) => {
+    // console.log("Login success!");
+    const redirectUrl = req.session.returnTo || '/';
+    // console.log(`Redirect URL is: ${redirectUrl}`);
+	delete req.session.returnTo;
+    res.redirect(redirectUrl);
+}
+
+module.exports.logoutUser = (req, res) => {
+    req.logout();
+    // console.log("Logout success!");
+    res.redirect('/');
+}
--- src/routes/user.js
+++ src/routes/user.js
@@ -4,44 +4,18 @@ const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const { User } = require('../models/user');
 const { isLoggedIn } = require("../utils/loginMiddleware");
+const user = require("../controllers/user");
 
-router.get('/register', (req, res) => {
-    res.render('users/register');
-});
+router.route("/register")
+	.get(user.renderRegisterForm)
+	.post(catchAsync(user.registerUser));
 
-router.post('/register', catchAsync(async (req, res, next) => {
-    try {
-        const { email, username, password } = req.body;
-        const user = new User({ email, username }); // Failing here
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {
-            if (err) return next(err);
-			// console.log("Register success!");
-            res.redirect('/');
-        })
-    } catch (e) {
-		// console.log("Register failure!");
-        res.redirect('/register');
-    }
-}));
+router.route("/login")
+	.get(user.renderLoginForm)
+	.post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), user.loginUser);
 
-router.get('/login', (req, res) => {
-    res.render('users/login');
-})
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-    // console.log("Login success!");
-    const redirectUrl = req.session.returnTo || '/';
-    // console.log(`Redirect URL is: ${redirectUrl}`);
-	delete req.session.returnTo;
-    res.redirect(redirectUrl);
-})
-
-router.get('/logout', (req, res) => {
-    req.logout();
-    // console.log("Logout success!");
-    res.redirect('/');
-})
+router.route("/logout")
+	.get(user.logoutUser);
 
 
 module.exports = router;
